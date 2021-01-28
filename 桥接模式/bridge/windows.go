9022d1dcabc4aab95094f8466132f34cbbe58d85
leichaojian <leichaojian@outlook.com>
2021-01-28 21:40:58 +0800

    桥接模式

---
+++ 桥接模式/bridge/windows.go
@@ -0,0 +1,15 @@
+package bridge
+
+import "fmt"
+type Windows struct {
+	printer printer
+}
+
+func (w *Windows) Print() {
+	fmt.Println("Print request for windows")
+	w.printer.PrintFile()
+}
+
+func (w *Windows) SetPrinter(p printer) {
+	w.printer = p
+}
---
+++ 桥接模式/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+import bridge "p4/bridge"
+
+func main() {
+	hpPrinter:=&bridge.Hp{}
+	epsonPrinter := &bridge.Epson{}
+
+	macComputer := &bridge.Mac{}
+
+	macComputer.SetPrinter(hpPrinter)
+	macComputer.Print()
+	fmt.Println()
+
+	macComputer.SetPrinter(epsonPrinter)
+    macComputer.Print()
+    fmt.Println()
+
+    winComputer := &bridge.Windows{}
+
+    winComputer.SetPrinter(hpPrinter)
+    winComputer.Print()
+    fmt.Println()
+
+    winComputer.SetPrinter(epsonPrinter)
+    winComputer.Print()
+    fmt.Println()
+}
