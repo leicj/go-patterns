1efa369bd2b9ca582af1afc3f168bf21f09f9f8e
leichaojian <leichaojian@outlook.com>
2021-02-02 20:25:26 +0800

    备忘录

---
+++ 备忘录/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	memento "p17/memento"
+)
+
+func main() {
+	caretaker := &memento.Caretaker{
+		MementoArray: make([]*memento.Memento, 0),
+	}
+
+	originator := &memento.Originator{
+		State: "A",
+	}
+
+	fmt.Printf("Originator Current State: %s\n", originator.GetState())
+	caretaker.AddMemento(originator.CreateMemento())
+
+	originator.SetState("B")
+	fmt.Printf("Originator Current State: %s\n", originator.GetState())
+	caretaker.AddMemento(originator.CreateMemento())
+
+	originator.SetState("C")
+	fmt.Printf("Originator Current State: %s\n", originator.GetState())
+	caretaker.AddMemento(originator.CreateMemento())
+
+	originator.Restorememento(caretaker.GetMemento(1))
+	fmt.Printf("Restored to State: %s\n", originator.GetState())
+
+	originator.Restorememento(caretaker.GetMemento(0))
+	fmt.Printf("Restored to State: %s\n", originator.GetState())
+}
