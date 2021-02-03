fee667fafa8d1a6fd3dde2686bda960fbc139801
leichaojian <leichaojian@outlook.com>
2021-02-03 20:54:45 +0800

    访问者

---
+++ 访问者/visitor/rectangle.go
@@ -0,0 +1,14 @@
+package visitor
+
+type Rectangle struct {
+	L int
+	B int
+}
+
+func (t *Rectangle) Accept(v Visitor) {
+	v.VisitForrectangle(t)
+}
+
+func (t *Rectangle) GetType() string {
+	return "rectangle"
+}
